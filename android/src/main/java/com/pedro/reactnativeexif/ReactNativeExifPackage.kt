15fedb7a225a52b7eea7fcdb3a43e63a98f23ca1
Pedro Gabriel <pedrpg5@gmail.com>
2025-10-03 15:01:41 -0300

    feat(lib): adiciona função para extrair EXIF

--- android/src/main/java/com/pedro/reactnativeexif/ReactNativeExifPackage.kt
+++ android/src/main/java/com/pedro/reactnativeexif/ReactNativeExifPackage.kt
@@ -8,26 +8,25 @@ import com.facebook.react.module.model.ReactModuleInfoProvider
 import java.util.HashMap
 
 class ReactNativeExifPackage : BaseReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == ReactNativeExifModule.NAME) {
-      ReactNativeExifModule(reactContext)
-    } else {
-      null
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
+        return if (name == ReactNativeExifModule.NAME) {
+            ReactNativeExifModule(reactContext)
+        } else {
+            null
+        }
     }
-  }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      moduleInfos[ReactNativeExifModule.NAME] = ReactModuleInfo(
-        ReactNativeExifModule.NAME,
-        ReactNativeExifModule.NAME,
-        false,  // canOverrideExistingModule
-        false,  // needsEagerInit
-        false,  // isCxxModule
-        true // isTurboModule
-      )
-      moduleInfos
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
+        return ReactModuleInfoProvider {
+            moduleInfos[ReactNativeExifModule.NAME] = ReactModuleInfo(
+                ReactNativeExifModule.NAME,
+                ReactNativeExifModule.NAME,
+                false,  // canOverrideExistingModule
+                false,  // needsEagerInit
+                false,  // isCxxModule
+                true // isTurboModule
+            )
+            moduleInfos
+        }
     }
-  }
 }
